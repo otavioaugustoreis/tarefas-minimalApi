345894a22c310a6fa2639d55f79eb701a89d5083
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-01-30 13:03:41 -0300

    feat: Criando a classe TarefaEntity e configurando o Bd

---
+++ MinimalApiTarefas/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiTarefas.Entities;
+
+namespace MinimalApiTarefas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+        public DbSet<TarefaEntity> Tarefas => Set<TarefaEntity>();
+    }
+}
